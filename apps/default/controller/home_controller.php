e5d36524c58021ac66efe2ba30bea9281fd0232a
Blas Soto <blassoto@gmail.com>
2013-04-18 22:21:53 -0400

    optimiacion y modificacion total de QuiqueModel por mejoras

---
+++ apps/default/controller/home_controller.php
@@ -0,0 +1,8 @@
+<?php
+class home_controller extends Controller {
+
+    public function index() {
+        $this->view("index");
+    }
+
+}
---
+++ apps/default/view/home/index.php
@@ -0,0 +1,3 @@
+<h1>Welcome to QuiquePHP</h1>
+Desde el directorio de tu proyecto ejecuta:<br />
+<b>php bin/quique.php</b>
